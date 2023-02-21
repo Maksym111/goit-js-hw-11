d8379c18fb01a8d5122eadb50215f109506750d7
Maksym Kikot <maxym1111111@gmail.com>
2023-02-21 16:11:22 +0200

    full hw

--- src/js/search-form.js
+++ src/js/search-form.js
@@ -1,58 +1,55 @@
+import throttle from 'lodash.throttle';
 import ImagesApiService from './server';
 import createElem from './create-elements';
-import LoadMoreBtn from './load-more-btn';
 
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-import InfiniteScroll from 'infinite-scroll';
-
 const refs = {
   searchForm: document.querySelector('#search-form'),
   galleryEl: document.querySelector('.gallery'),
+  sentinel: document.querySelector('#sentinel'),
   scrollBtn: document.querySelector('[data-action="scroll-up"]'),
 };
 
 const imagesApiService = new ImagesApiService();
-const loadMoreBtnEl = new LoadMoreBtn({
-  selector: '[data-action="load-more-js"]',
-  hidden: true,
-});
+
 const imageLightBox = new SimpleLightbox('.gallery a', {
   scrollZoom: false,
 });
 
 refs.searchForm.addEventListener('submit', onSearchForm);
-loadMoreBtnEl.refs.button.addEventListener('click', onLoadMoreClick);
+window.addEventListener('scroll', throttle(toFromBtnScroll, 500));
 refs.scrollBtn.addEventListener('click', onScrollUpClick);
 
 async function onSearchForm(e) {
   e.preventDefault();
 
+  observer.unobserve(sentinel);
+  observer.observe(sentinel);
+
   imagesApiService.query = e.currentTarget.elements.searchQuery.value;
 
-  loadMoreBtnEl.show();
-  loadMoreBtnEl.disable();
+  // loadMoreBtnEl.show();
+  // loadMoreBtnEl.disable();
 
   clearGallery();
   imagesApiService.resetPage();
-
-  await insertMarcup();
-
-  imageLightBox.refresh();
 }
 
 async function insertMarcup() {
   const elements = await imagesApiService.fetchRequest();
-
   if (elements === undefined || elements.length === 0) {
-    loadMoreBtnEl.hide();
+    // loadMoreBtnEl.hide();
     return;
   }
   refs.galleryEl.insertAdjacentHTML('beforeend', createElem(elements));
+  // loadMoreBtnEl.enable();
+}
 
-  loadMoreBtnEl.enable();
-  refs.scrollBtn.classList.remove('btn-hide');
+async function addMoreMarcup() {
+  await insertMarcup();
+  imageLightBox.refresh();
 }
 
 function clearGallery() {
@@ -60,32 +57,52 @@ function clearGallery() {
   refs.scrollBtn.classList.add('btn-hide');
 }
 
-async function onLoadMoreClick() {
-  loadMoreBtnEl.disable();
-  await insertMarcup();
-  imageLightBox.refresh();
+function onScrollUpClick(e) {
+  // const { height: cardHeight } = document
+  //   .querySelector('.gallery')
+  //   .firstElementChild.getBoundingClientRect();
+  // window.scrollBy({
+  //   top: cardHeight * 2.15,
+  //   behavior: 'smooth',
+  // });
+
+  const form = refs.searchForm;
+  form.scrollIntoView({ behavior: 'smooth' });
 }
 
-function onScrollUpClick() {
-  const { height: cardHeight } = document
-    .querySelector('.gallery')
-    .firstElementChild.getBoundingClientRect();
-
-  window.scrollBy({
-    top: cardHeight * 2.15,
-    behavior: 'smooth',
+function onEntry(entries) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      addMoreMarcup();
+    }
   });
 }
 
-// let infScroll = new InfiniteScroll(refs.galleryEl, {
-//   path: getPenPath,
-//   append: '.post',
-//   history: false,
+function toFromBtnScroll() {
+  if (window.pageYOffset > 1000) {
+    refs.scrollBtn.classList.remove('btn-hide');
+  } else {
+    refs.scrollBtn.classList.add('btn-hide');
+  }
+}
+
+const options = {
+  rootMargin: '200px',
+};
+const observer = new IntersectionObserver(onEntry, options);
+
+//Добавление картинок по кнопке, вместо бесконечного скрола
+// import LoadMoreBtn from './load-more-btn';
+
+// const loadMoreBtnEl = new LoadMoreBtn({
+//   selector: '[data-action="load-more-js"]',
+//   hidden: true,
 // });
 
-// function getPenPath() {
-//   let slug = a[this.loadCount];
-//   if (slug) {
-//     return `/desandro/debug/${slug}`;
-//   }
+// loadMoreBtnEl.refs.button.addEventListener('click', onLoadMoreClick);
+
+// async function onLoadMoreClick() {
+//   loadMoreBtnEl.disable();
+//   await insertMarcup();
+//   imageLightBox.refresh();
 // }
